0062dbfef470e0679aaba922c64e4223900ee5ed
Zubair Shaikh <zsescape@gmail.com>
2019-09-16 14:54:42 -0400

    Modify postman install and update scripts

--- update_postman.sh
+++ update_postman.sh
@@ -1,5 +1,9 @@
 echo "Downloading Postman..."
 wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+echo "Removing old installations..."
+rm -rf /opt/Postman
+rm /usr/bin/postman
+rm -rf ~/.config/Postman
 echo "Extracting Postman..."
 sudo tar -xzf postman.tar.gz -C /opt
 rm postman.tar.gz
