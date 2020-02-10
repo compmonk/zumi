634ebbb9cd6fd6580d4185844d90fbc6e4748cac
compmonk <zsescape@gmail.com>
2020-02-10 00:23:27 -0500

    Change Robot 3t icon

--- install_robo3t.sh
+++ install_robo3t.sh
@@ -3,7 +3,7 @@ wget https://download.robomongo.org/1.2.1/linux/robo3t-1.2.1-linux-x86_64-3e50a6
 echo "Extracting Robo3T..."
 sudo tar -xzf robo3t.tar.gz -C /opt/
 sudo mv /opt/robo3t-1.2.1-linux-x86_64-3e50a65 /opt/robo3t
-wget http://mongodb-tools.com/img/robomongo.png -O /opt/robo3t/robomongo.png
+wget https://github.com/shalev67/Robo3T-snap/blob/master/snap/gui/icon.png -O /opt/robo3t/icon.png
 #rm robo3t.tar.gz
 sudo ln -sf /opt/robo3t/bin/robo3t /usr/bin/robo3t
 
@@ -13,7 +13,7 @@ cat > ~/.local/share/applications/robo3t.desktop <<EOL
 Encoding=UTF-8
 Name=Robo3T
 Exec=robo3t
-Icon=/opt/robo3t/robomongo.png
+Icon=/opt/robo3t/icon.png
 Terminal=false
 Type=Application
 Categories=Development;
