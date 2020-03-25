8f915d6b98e4340e23d8bc1eff8aa4803ee41cee
compmonk <zsescape@gmail.com>
2020-03-25 15:55:49 -0400

    Update install_pgadmin.sh

--- install_pgadmin.sh
+++ install_pgadmin.sh
@@ -1,7 +1,7 @@
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
-sudo apt-get install wget ca-certificates
+sudo apt-get install -y wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install pgadmin4
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y pgadmin4
